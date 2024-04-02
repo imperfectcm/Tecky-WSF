6775fa8c7c521a827124de7de7b26b9b59d08cd9
imperfectcm <imperfect.cm@gmail.com>
2024-04-03 00:35:17 +0800

    2024.04.02A

--- memoWall/main.ts
+++ memoWall/main.ts
@@ -4,6 +4,29 @@ import path from 'path';
 import expressSession from "express-session";
 
 const app = express();
+let count = 0;
+let date_time = new Date();
+// get current date
+// adjust 0 before single digit date
+let date = ("0" + date_time.getDate()).slice(-2);
+
+// get current month
+let month = ("0" + (date_time.getMonth() + 1)).slice(-2);
+
+// get current year
+let year = date_time.getFullYear();
+
+// get current hours
+let hours = date_time.getHours();
+
+// get current minutes
+let minutes = date_time.getMinutes();
+
+// get current seconds
+let seconds = date_time.getSeconds();
+
+// prints date & time in YYYY-MM-DD HH:MM:SS format
+let currentTime = (year + "-" + month + "-" + date + " " + hours + ":" + minutes + ":" + seconds);
 
 app.use(
     expressSession({
@@ -15,18 +38,30 @@ app.use(
 
 declare module "express-session" {
     interface SessionData {
-        name?: string;
+        name?: String;
+        counter?: Number;
     }
 }
 
 app.use((req, res, next) => {
-    console.log("hi",req.session,req.path)
+    // console.log("hi", req.session, req.path)
+    count++;
+    req.session.counter = count;
+    // console.log("hi", req.session);
     next()
+    return count;
 })
 
 app.get('/hello', function (req: Request, res: Response) {
     res.end("Hello World2")
 })
+
+app.use((req, res, next) => {
+    console.log([currentTime],` Request `,req.path)
+    next()
+})
+
+
 
 app.use(express.static('public'))
 
